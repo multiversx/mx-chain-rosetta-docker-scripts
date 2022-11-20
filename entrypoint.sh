149c3ff2e650ecffd07f247f3c3ee3b0e633702e
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2022-11-20 15:12:12 +0200

    Further simplification.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,30 +1,13 @@
 #!/bin/bash
 
-NETWORK=""
-PROGRAM=""
 ARGS=$@
 
+echo "Network: ${NETWORK}"
+echo "Program: ${PROGRAM}"
+echo "Args: ${ARGS}"
+
 # Decide program to run
-if [[ $@ == *"start-observer"* ]]; then
-    ARGS="${ARGS//start-observer/}"
-
-    # For Node, decide network
-    if [[ $@ == *"network=mainnet"* ]]; then
-        NETWORK=mainnet
-        ARGS="${ARGS//network=mainnet/}"
-    elif [[ $@ == *"network=devnet"* ]]; then
-        NETWORK=devnet
-        ARGS="${ARGS//network=devnet/}"
-    elif [[ $@ == *"network=testnet"* ]]; then
-        NETWORK=testnet
-        ARGS="${ARGS//network=testnet/}"
-    else
-        echo "Error: unknown network switch." 1>&2
-        exit 1
-    fi
-
-    echo "Network: ${NETWORK}"
-
+if [[ ${PROGRAM} == "node" ]]; then
     # For Node, create observer key (if missing)
     if [ ! -f "/data/observerKey.pem" ]
     then
@@ -35,28 +18,19 @@ if [[ $@ == *"start-observer"* ]]; then
         echo "Observer key already existing."
     fi
 
-    # For Node, decide current directory, executable and export LD_LIBRARY_PATH (mainnet vs. devnet vs. testnet)
-    DIRECTORY=/elrond/${NETWORK}
-    PROGRAM=${DIRECTORY}/node
-    export LD_LIBRARY_PATH=${DIRECTORY}
-
     # For Node, check existence of /data/db
     if [ ! -d "/data/db" ]; then
         echo "Make sure the directory /data/db exists and contains a (recent) blockchain archive." 1>&2
         exit 1
     fi
-elif [[ $@ == *"start-rosetta"* ]]; then
-    DIRECTORY=/elrond
-    PROGRAM=${DIRECTORY}/rosetta
-    ARGS="${ARGS//start-rosetta/}"
-else
-    echo "Error: unknown program." 1>&2
-    exit 1
 fi
+
+DIRECTORY=/elrond/${NETWORK}
+export LD_LIBRARY_PATH=${DIRECTORY}
 
 # Run the main process:
 cd ${DIRECTORY}
 echo "Program: ${PROGRAM}"
 echo "Command-line arguments: ${ARGS}"
 echo "LD_LIBRARY_PATH: ${LD_LIBRARY_PATH}"
-exec ${PROGRAM} ${ARGS}
+exec ./${PROGRAM} ${ARGS}
