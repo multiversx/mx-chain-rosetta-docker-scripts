d1bb627ef3c47009b7d130c661f7e02b96fd95c0
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2022-11-20 15:36:16 +0200

    Update comments.

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,9 @@ echo "Network: ${NETWORK}"
 echo "Program: ${PROGRAM}"
 echo "Args: ${ARGS}"
 
-# Decide program to run
+# For Node (observer), perform additional steps
 if [[ ${PROGRAM} == "node" ]]; then
-    # For Node, create observer key (if missing)
+    # Create observer key (if missing)
     if [ ! -f "/data/observerKey.pem" ]
     then
         /elrond/keygenerator
@@ -18,7 +18,7 @@ if [[ ${PROGRAM} == "node" ]]; then
         echo "Observer key already existing."
     fi
 
-    # For Node, check existence of /data/db
+    # Check existence of /data/db
     if [ ! -d "/data/db" ]; then
         echo "Make sure the directory /data/db exists and contains a (recent) blockchain archive." 1>&2
         exit 1
